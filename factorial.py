86fbc3566393876d5e1a3c1cd725070f5907e304
Angel Irangabiye <angel.irangabiye@stu.ocsb.ca>
2025-04-17 16:51:39 +0000

    Added factorial.py

--- factorial.py
+++ factorial.py
@@ -5,27 +5,28 @@
 # it then uses a do..while loop to calculate the
 # factorial of the number.
 
-def main ():
+
+def main():
     # initializations
     loop_counter = 0
     factorial_answer = 1
-     
-    # get the user number
+
+    # get the user number.
     user_number = int(input("Enter a positive number: "))
     print("")
 
     # replicates a do..while loop
     # Calculate the factorial of the user number using a loop
     while True:
-      loop_counter = loop_counter +1
-      factorial_answer + factorial_answer * loop_counter
-      print("Tracking {} times through loop.".format
-      (loop_counter))
-      if loop_counter >= user_number:
-        break
-      
+        loop_counter = loop_counter + 1
+        factorial_answer + factorial_answer * loop_counter
+        print("Tracking {} times through loop.".format(loop_counter))
+        if loop_counter >= user_number:
+            break
+
     print("")
-    print ("{}! = {}".format(user_number,factorial_answer))
+    print("{}! = {}".format(user_number, factorial_answer))
+
 
 if __name__ == "__main__":
-   main()
+    main()
